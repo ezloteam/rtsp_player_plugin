c6b762d69fb01b09f31beecce21f8d5243f2cc9a
Max Vitruk <maxvitruk@gmail.com>
2019-08-20 16:16:17 +0300

    ios part init player stage 0.1

--- src/rtsp-player.ios.ts
+++ src/rtsp-player.ios.ts
@@ -3,24 +3,31 @@ import { CreateViewEventData } from "tns-core-modules/ui/placeholder";
 
 export class RtspPlayer extends AbstractRtspPlayer {
 
+    private playerView: any;
+
     constructor(args: CreateViewEventData) {
         super(args);
-    }  
-    
+
+        let frame = new CGRect(0,0,200,100);
+        let opts = NSDictionary.alloc();
+        this.playerView = EzloRTSPView.initWithContentPathParametersFrame("rtsp://admin:admin123@192.168.30.73",opts,frame);
+        args.view = this.playerView;
+    }
+
     public startStream(streamUrl: string) {
-        throw new Error("Method not implemented.");
+
     }
 
     public stopStream() {
-        throw new Error("Method not implemented.");
+        this.playerView.pause();
     }
 
     public release() {
-        throw new Error("Method not implemented.");
+        this.playerView.useMemoryWarning();
     }
 
     public isStreaming() : boolean {
-        throw new Error("Method not implemented.");
+        return this.playerView.playing;
     }
 
 }
